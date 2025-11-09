aa04bb7bc2d89c9e83133104f94c796ad00b540a
kimmie38 <kimrwang38@gmail.com>
2025-11-09 21:15:58 +0100

    Mining

--- app/mining/page.tsx
+++ app/mining/page.tsx
@@ -7,7 +7,7 @@ export default function MiningPage() {
       <section className="bg-[#FBEAE957] py-12">
         <div className="max-w-7xl mx-auto px-6">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6 items-center">
-            <h1 className="text-3xl md:text-4xl font-bold text-red-600">Mining</h1>
+            <h1 className="relative inline-block text-3xl md:text-4xl font-bold text-red-600 after:content-[''] after:block after:w-35 after:h-[1px] after:bg-red-600 after:mt-2">Mining</h1>
             <p className="text-sm text-gray-700">
               Advancing responsible mining operations that unlock Nigeria’s rich mineral potential while promoting sustainability and community growth.
             </p>
